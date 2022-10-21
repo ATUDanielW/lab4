7b98e50b40bf51c17ef02880d0b825889ca09cf3
Daniel <G00386720@atu.ie>
2022-10-21 09:22:53 +0000

    lab 4

--- src/components/create.js
+++ src/components/create.js
@@ -1,12 +1,92 @@
 import React from "react";
 
-export class Create extends React.Component{
+export class Create extends React.Component {
 
-    render(){
-        return(
+    constructor() {
+
+        //Parent class constructor
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChangeBookTitle = this.onChangeBookTitle.bind(this);
+        this.onChangeBookCover = this.onChangeBookCover.bind(this);
+        this.onChangeBookAuthor = this.onChangeBookAuthor.bind(this);
+
+
+        this.state = {
+            title: '',
+            cover: '',
+            author: ''
+        }
+    }
+
+    //implementing handle submit
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(`${this.state.title}`);
+
+    }
+    //changes current value from render
+    onChangeBookTitle(e) {
+        this.setState({
+            title: e.target.value
+        })
+    }
+
+    //changes current value from render
+    onChangeBookCover(e) {
+        this.setState({
+            cover: e.target.value
+        })
+    }
+
+    //changes current value from render
+    onChangeBookAuthor(e){
+        this.setState({
+            author: e.target.value
+        })
+    }    
+
+    render() {
+        return (
             <div>
                 <h3>hello from my Create component</h3>
-            </div>
+                {/*event*/}
+                <form onSubmit={this.handleSubmit}>
+
+                    {/* The application should handle the new events from the button click and log the information submitted */}
+
+                    <div className="form-group">
+                        <label>Add Book Title: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.title}
+                            onChange={this.onChangeBookTitle}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Add Book Cover: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.cover}
+                            onChange={this.onChangeBookCover}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Add Book Author: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.author}
+                            onChange={this.onChangeBookAuthor}
+                        />
+                    </div>
+
+
+                    {/* this has value submitt will apear as button */}
+                    <input type="submit" value="Add Book" />
+                </form >
+            </div >
         );
     }
 }
